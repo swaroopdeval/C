fd3ee1bdfe5f77bfd54bd78d0c17d3a77c2244a1
Swaroop Singh Deval <ssinghdeval@gmail.com>
2014-09-09 11:15:43 +0530

    Create Counting_Sort_2.c

---
+++ HackerRank/Algorithms/Arrays-And-Sorting/Easy/Counting-Sort-2/Counting_Sort_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i,arr[100]={0};
+    scanf("%d",&n);
+    
+    while(n!=0){
+        scanf("%d",&i);
+        arr[i]++;
+        n--;
+    }
+    for(i=0;i<100;i++){
+        if(arr[i]==0) continue;
+        printf("%d ",i);
+        arr[i--]--;
+    }
+    printf("\n");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
