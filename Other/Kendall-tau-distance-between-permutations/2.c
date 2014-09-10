fd3683ab9e283011b302377b0e1a0643878abce5
Swaroop Singh Deval <ssinghdeval@gmail.com>
2014-09-10 21:12:37 +0530

    Create 2.c

---
+++ Other/Kendall-tau-distance-between-permutations/2.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+int count=0;
+void part(int [],int ,int);
+void merge(int [],int,int,int);
+int rank[100]={0};
+int main(){
+
+  int n,i;
+  scanf("%d",&n);
+  int ar[100];
+  int temp;
+  for(i=0;i<n;i++){
+   scanf("%d",&temp);
+   rank[temp]=i;
+  }
+  for(i=0;i<n;i++) scanf("%d",&ar[i]);
+
+  part(ar,0,n-1);
+  
+  printf("%d\n",count);
+  return 0;
+}
+
+void part(int arr[],int min,int max)
+{
+ int mid;
+ if(min<max)
+ {
+   mid=(min+max)/2;
+   part(arr,min,mid);
+   part(arr,mid+1,max);
+   merge(arr,min,mid,max);
+ }
+}
+
+void merge(int arr[],int min,int mid,int max)
+{
+  int tmp[30];
+  int i,j,k,m; 
+  j=min;
+  m=mid+1;
+  for(i=min; j<=mid && m<=max ; i++)
+  {
+     if(rank[arr[j]]<=rank[arr[m]])
+     {
+         tmp[i]=arr[j];
+         j++;
+     }
+     else
+     {
+         tmp[i]=arr[m];
+         m++;
+         count=count+mid-j+1;
+     }
+  }
+  if(j>mid)
+  {
+     for(k=m; k<=max; k++)
+     {
+         tmp[i]=arr[k];
+         i++;
+     }
+  }
+  else
+  {
+     for(k=j; k<=mid; k++)
+     {
+        tmp[i]=arr[k];
+        i++;
+     }
+  }
+  for(k=min; k<=max; k++)
+     arr[k]=tmp[k];
+}
